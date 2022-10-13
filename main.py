07ea0cf4cae5f8b87dea613cfd39b7f9151ebd82
unknown <yasenyankov21@gmail.com>
2022-10-13 16:30:12 +0300

    added schedule functionality

--- main.py
+++ main.py
@@ -2,69 +2,31 @@ import os
 import datetime
 from datetime import date
 import calendar
+import time as t
 
-command="mplayer schoolbell_yordan.mp3"
 
-while True:
-    my_date = date.today()
-    day=calendar.day_name[my_date.weekday()]
-    if day!="Sunday" and day!="Saturday":
-        now = datetime.datetime.now().strftime('%H:%M')
-        print(now,day)
-        match now:
-            case "07:30":
+schedule=[]
+def sleep(x):
+    for x in range(x):
+        t.sleep(1)
+
+def main():
+    command="mplayer schoolbell_yordan.mp3"
+    f = open("schedule.txt", "r")
+    for time in f:
+        schedule.append(time.replace("\n",''))
+    while True:
+        my_date = date.today()
+        day=calendar.day_name[my_date.weekday()]
+        if day!="Sunday" and day!="Saturday":
+            now = datetime.datetime.now().strftime('%H:%M:%S')
+            if now in schedule:
                 os.system(command)
-            case "08:10":
-                os.system(command)                
-            case "08:20":
-                os.system(command)
-            case "09:00":
-                os.system(command)     
-            case "09:40":
-                os.system(command)
-            case "09:50":
-                os.system(command)   
-            case "10:10":
-                os.system(command)  
-            case "10:50":
-                os.system(command)          
-            case "11:00":
-                os.system(command) 
-            case "11:40":
-                os.system(command) 
-            case "11:45":
-                os.system(command) 
-            case "12:25":
-                os.system(command) 
-            case "12:30":
-                os.system(command) 
-            case "13:10":
-                os.system(command) 
-            case "13:20":
-                os.system(command) 
-            case "14:00":
-                os.system(command) 
-            case "14:10":
-                os.system(command) 
-            case "14:50":
-                os.system(command) 
-            case "15:00":
-                os.system(command) 
-            case "15:40":
-                os.system(command) 
-            case "16:00":
-                os.system(command) 
-            case "16:40":
-                os.system(command) 
-            case "16:50":
-                os.system(command) 
-            case "17:30":
-                os.system(command) 
-            case "17:35":
-                os.system(command) 
-            case "18:15":
-                os.system(command) 
-            case "18:20":
-                os.system(command) 
-            case "19:00":
-                os.system(command) 
+                sleep(2)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
