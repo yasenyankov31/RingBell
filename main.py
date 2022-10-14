4dd7617ea4a71f28e16d802b3ea96deb404f5cf9
unknown <yasenyankov21@gmail.com>
2022-10-14 11:49:55 +0300

    break song functionality added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import os 
 import datetime
 from datetime import date
 import calendar
 import time as t
 import subprocess
 import multiprocessing as mp
+import os
+import random
+from mutagen.mp3 import MP3
+
 
 
 
@@ -12,10 +15,20 @@ def sleep(x):
     for x in range(x):
         t.sleep(1)
 
+def play_song(command):
+    subprocess.call(command, shell=True)
+
+def get_all_songs():
+    songs = os.listdir("songs/") 
+    return songs
+
 def main():
     schedule=[]
-    command="mplayer /songs/schoolbell_yordan.mp3"
+    command_bell="mplayer songs/bell.mp3"
+    command_song="mplayer "
+    all_songs=get_all_songs()
     prevhour=""
+    counter=0
     #fill schedule list
     f = open("schedule.txt", "r")
     for time in f:
@@ -26,23 +39,43 @@ def main():
         day=calendar.day_name[my_date.weekday()]
         if day!="Sunday" and day!="Saturday":
             now = datetime.datetime.now().strftime('%H:%M')
+            print(now)
             if now in schedule and now!=prevhour:
+                counter+=1
                 prevhour=now
-                os.system(command)
+                subprocess.call(command_bell,shell=True)
+                sleep(5)
+                if counter==6:
+                    breakSongs={}
+                    fullDuration=0
+                    #add random songs in list
+                    while True:
+                        rng_song=command_song+random.choice(all_songs)
+                        duration = int(MP3(rng_song).info.length)
+                        if fullDuration+duration<=1200:
+                            duration=1200-fullDuration
+                            breakSongs[rng_song]=str(duration-20)
+                            break
+                        breakSongs[rng_song]=str(duration)
+
+                    for song, duration in breakSongs.items():
+                        task=mp.Process(target=play_song,args=(song,))
+                        task.start()
+                        sleep(duration)
+                        subprocess.call('pkill mplayer', shell=True)
+                        task.kill()
+                        
+                elif counter==14:
+                    counter=0
                 #write log
                 w=open("log.txt","a")
                 w.write(now+" "+str(my_date)+"\n")
                 w.close()
         sleep(10)
 
-def play_song():
-    subprocess.call('cd mplayer && mplayer songs/song.mp3', shell=True)
+
 
 
 if __name__ == "__main__":
-    # task1=mp.Process(target=play_song)
-    # task1.start()
-    # sleep(5)
-    # subprocess.call('taskkill /f /im mplayer.exe', shell=True)
-    # print("end")
     main()
+    
